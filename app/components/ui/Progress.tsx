f3af8646099b725315f75b8a5657e5aaf925b946
lewin <Ephraim-Nkhoma>
2026-03-05 23:39:01 +0200

    Your commit message here

--- app/components/ui/Progress.tsx
+++ app/components/ui/Progress.tsx
@@ -2,6 +2,7 @@
 
 import * as React from 'react'
 import * as ProgressPrimitive from '@radix-ui/react-progress'
+import { CheckCircle, XCircle } from 'lucide-react' // Add these imports
 import { cn } from '../../lib/utils'
 
 interface ProgressProps extends React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root> {
